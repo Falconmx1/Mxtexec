8de37dcfc20a1866943b0e320eeff55a330a05b4
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-06-04 04:11:38 -0600

    Update banner.py

--- mxc/core/banner.py
+++ mxc/core/banner.py
@@ -1,20 +1,23 @@
+from mxc.core.color import Colors
+
 def show_banner():
-    banner = r"""
-    .----------------.     _   _          _     _____
-   | .--------------. |   | \ | |   ___  | |_  | ____| __  __   ___    ___
-   | |   .------.   | |   |  \| |  / _ \ | __| |  _|   \ \/ /  / _ \  / __|
-   | |  |  ()() |   | |   | |\  | |  __/ | |_  | |___   >  <  |  __/ | (__
-   | |  |  (__) |   | |   |_| \_|  \___|  \__| |_____| /_/\_\ \___|  \___|
+    banner = f"""
+{Colors.CYAN}    .----------------.     {Colors.WHITE}_   _          _     _____{Colors.CYAN}
+   | .--------------. |   {Colors.WHITE}| \\ | |   ___  | |_  | ____| __  __   ___    ___{Colors.CYAN}
+   | |   .------.   | |   {Colors.WHITE}|  \\| |  / _ \\ | __| |  _|   \\ \\/ /  / _ \\  / __|{Colors.CYAN}
+   | |  |  ()() |   | |   {Colors.WHITE}| |\\  | |  __/ | |_  | |___   >  <  |  __/ | (__{Colors.CYAN}
+   | |  |  (__) |   | |   {Colors.WHITE}|_| \\_|  \\___|  \\__| |_____| /_/\\_\\ \\___|  \\___|{Colors.CYAN}
    | |  '------'   | |
    | '--------------' |
    '----------------'
-          .--.
-         /    \
+{Colors.GREEN}          .--.
+         /    \\
          |oo  |
          |_<  |
-         \____/
-          '  '
+         \\____/
+          '  '{Colors.RESET}
 
-    🐢 Mxtexec (mxc) - v0.1 - Caparazón letal 🐢
-    """
+{Colors.BOLD}{Colors.MAGENTA}    🐢 Mxtexec (mxc) - v0.1 - Caparazón letal 🐢{Colors.RESET}
+{Colors.YELLOW}    >>> Rápido, sigiloso y con todo el poder de una tortuga ninja <<<{Colors.RESET}
+"""
     print(banner)
